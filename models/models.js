a74c731813bfde17c4720b08232b0b7f5d8f7b03
jukav <jcsanchezv@msn.com>
2015-06-04 23:43:58 +0200

    mod07_2

--- models/models.js
+++ models/models.js
@@ -45,11 +45,13 @@ exports.Quiz = Quiz; // exporta definición de tabla Quiz
 
 // sequelize.sync().sucess(function(){
 sequelize.sync().then(function(){
-  // sucess(..) ejecuta el manejador una vez creada la tabla
+  // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function(count){
     if(count===0){ // la tabla se inicializa sólo si está vacía
-      Quiz.create({ pregunta: '¿Capital de Italia?', respuesta: 'Roma'})
-    .then(function(){console.log('Base de datos inicializada')});
-  };
-});
+      // Quiz.create({ pregunta: '¿Capital de Italia?', respuesta: 'Roma'})
+      Quiz.create({pregunta: '¿Capital de Italia?', respuesta: 'Roma'});
+      Quiz.create({pregunta: '¿Capital de Portugal?', respuesta: 'Lisboa'})
+      .then(function(){console.log('Base de datos inicializada')});
+    };
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,20 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/*
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+*/
+
+// autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload: quizId
+
+// definición de rutas /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+// autor
 router.get('/author', quizController.author);
 
 module.exports = router;
