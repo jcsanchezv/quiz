842c92d45f811d10ce94f552318e75d24a422de4
jukav <jcsanchezv@msn.com>
2015-06-12 19:14:19 +0200

    Mod09_2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+var session = require('express-session');
 var routes = require('./routes/index');
 
 var app = express();
@@ -21,10 +22,23 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
+
+// Helpers dinámicos:
+app.use(function(req, res, next){
+    // guardar path en session.redir para después de loguearse
+    if(!req.path.match(/\/login|\/logout/)){
+        req.session.redir = req.path;
+    }
+
+    // hacer visible req.session en las vistas
+    res.locals.session = req.session;
+    next();
+});
 
 app.use('/', routes);
 
---
+++ controllers/session_controller.js
@@ -0,0 +1,43 @@
+// MW autorización accesos HTTP restringidos
+exports.loginRequired = function(req, res, next){
+	if(req.session.user){
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
+
+// GET /login --form login--
+exports.new = function(req, res){
+	var errors = req.session.errors || {};
+	req.session.errors = {};
+
+	res.render('sessions/new', {errors: errors});
+};
+
+// POST /login --crea sesión--
+exports.create = function(req, res){
+	var login = req.body.login;
+	var password = req.body.password;
+
+	var userController = require('./user_controller');
+	userController.autenticar(login, password, function(error, user){
+		if(error) { // si hay error devuelve mensajes de error de sesión
+			req.session.errors = [{"message": 'Compruebe '+error}];
+			res.redirect("/login");
+			return;
+		}
+
+		// crear req.session.user y guardar campos id y username
+		// la sesión se define por la existencia de req.session.user
+		req.session.user = {id:user.id, username:user.username};
+
+		res.redirect(req.session.redir.toString()); // redirecciona a path anterior
+	});
+};
+
+// DELETE /logout --destruye sesión--
+exports.destroy = function(req, res){
+	delete req.session.user;
+	res.redirect(req.session.redir.toString()); // redirecciona a path anterior
+};
